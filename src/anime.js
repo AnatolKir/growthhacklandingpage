409c00706402823673254d4a9b2cc9edb780268a
Rick Latona <ricklatona@Ricks-MacBook-Pro.local>
2025-05-29 06:30:22 -0300

    Update: Simplify build process and anime.js for demo

--- src/anime.js
+++ src/anime.js
@@ -1,14 +1,48 @@
-export { engine } from './engine.js';
-export { createTimer, Timer } from './timer.js';
-export { animate, JSAnimation } from './animation.js';
-export { createTimeline, Timeline } from './timeline.js';
-export { createAnimatable, Animatable } from './animatable.js';
-export { createDraggable, Draggable } from './draggable.js';
-export { createScope, Scope } from './scope.js';
-export { onScroll, ScrollObserver, scrollContainers } from './scroll.js';
-export { createSpring, Spring } from './spring.js';
-export { waapi, WAAPIAnimation } from './waapi.js';
-export { utils } from './utils.js';
-export { svg } from './svg.js';
-export { stagger } from './stagger.js';
-export { eases } from './eases.js';
+// Simple version of anime.js for demo purposes
+const anime = (function() {
+  'use strict';
+
+  function createAnimation(params) {
+    const targets = typeof params.targets === 'string' 
+      ? document.querySelectorAll(params.targets)
+      : params.targets;
+
+    const duration = params.duration || 1000;
+    const easing = params.easing || 'linear';
+    const loop = params.loop || false;
+    const direction = params.direction || 'normal';
+    
+    const animations = Array.from(targets).map(target => {
+      const animation = target.animate([
+        { transform: 'translate(0) rotate(0)' },
+        { 
+          transform: `translateX(${params.translateX || 0}px) rotate(${params.rotate || '0deg'})`
+        }
+      ], {
+        duration,
+        easing,
+        iterations: loop ? Infinity : 1,
+        direction
+      });
+
+      return animation;
+    });
+
+    return {
+      play() {
+        animations.forEach(a => a.play());
+      },
+      pause() {
+        animations.forEach(a => a.pause());
+      }
+    };
+  }
+
+  return createAnimation;
+})();
+
+if (typeof module !== 'undefined') {
+  module.exports = anime;
+} else {
+  window.anime = anime;
+}
